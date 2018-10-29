5a4cfb30aa3053faea624325e9ba3027c67a79b1
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 11:38:35 -1000

    Compl L01 Ex46 #02 OR

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -255,6 +255,11 @@ SELECT id
 2. Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000.
 */
 
+SELECT *
+  FROM orders
+  WHERE standard_qty = 0
+    AND (gloss_qty > 1000 OR poster_qty > 1000);
+
 /*
 3. Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.
 */
