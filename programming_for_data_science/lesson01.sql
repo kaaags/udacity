7daa20b6d8dc567cc1b0279e2383dd6f101a4926
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 11:13:13 -1000

    Compl L01 Ex46 #01 OR

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -239,3 +239,22 @@ SELECT *
   WHERE occurred_at BETWEEN '2016-01-01' AND '2017-01-01' AND
     channel IN ('organic', 'adwords')
   ORDER BY occurred_at DESC;
+
+--46. Quiz: AND or BETWEEN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the resulting table.
+*/
+
+SELECT id
+  FROM orders
+  WHERE gloss_qty >= 4000
+    OR poster_qty >= 4000;
+
+/*
+2. Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000.
+*/
+
+/*
+3. Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.
+*/
