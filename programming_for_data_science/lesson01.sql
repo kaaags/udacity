183b9094119b81d438926e66b56866de892357cb
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 10:04:49 -1000

    Compl L01 Ex40 #01 AND and BETWEEN

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -207,3 +207,23 @@ SELECT *
   FROM accounts
   WHERE name NOT LIKE '%s'
   ORDER BY name;
+
+--40. Quiz: AND or BETWEEN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0.
+*/
+
+SELECT *
+  FROM orders
+  WHERE standard_qty > 1000 AND
+    poster_qty = 0 AND
+    gloss_qty = 0;
+
+/*
+2. Using the accounts table find all the companies whose names do not start with 'C' and end with 's'.
+*/
+
+/*
+3. Use the web_events table to find all information regarding individuals who were contacted via organic or adwords and started their account at any point in 2016 sorted from newest to oldest.
+*/
