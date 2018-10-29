7ec294ce500c1b17aaabdff2af798888843046f2
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 07:49:36 -1000

    Compl L01 Ex18 #02 ORDER BY and edited #01

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -16,13 +16,17 @@ Write a query to return the 10 earliest orders in the orders table. Include the 
 
 SELECT id, occurred_at, total_amt_usd
   FROM orders
-  LIMIT 10
+  ORDER BY occurred_at
+  LIMIT 10;
 
 /*
 Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
 */
 
-
+SELECT id, account_id, total_amt_usd
+  FROM orders
+  ORDER BY total_amt_usd DESC
+  LIMIT 5;
 
 /*
 Write a query to return the bottom 20 orders in terms of least total. Include the id, account_id, and total.
