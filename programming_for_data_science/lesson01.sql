8899190795d7b578aa45e331bc1b65446eb1dfbe
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 08:12:53 -1000

    Compl L01 Ex21 #01 ORDER BY Part II

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -11,7 +11,7 @@ SELECT occurred_at, account_id, channel
 --18. ORDER BY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 /*
-Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
+1. Write a query to return the 10 earliest orders in the orders table. Include the id, occurred_at, and total_amt_usd.
 */
 
 SELECT id, occurred_at, total_amt_usd
@@ -20,7 +20,7 @@ SELECT id, occurred_at, total_amt_usd
   LIMIT 10;
 
 /*
-Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
+2. Write a query to return the top 5 orders in terms of largest total_amt_usd. Include the id, account_id, and total_amt_usd.
 */
 
 SELECT id, account_id, total_amt_usd
@@ -29,10 +29,25 @@ SELECT id, account_id, total_amt_usd
   LIMIT 5;
 
 /*
-Write a query to return the bottom 20 orders in terms of least total. Include the id, account_id, and total.
+3. Write a query to return the bottom 20 orders in terms of least total. Include the id, account_id, and total.
 */
 
 SELECT id, account_id, total
   FROM orders
   ORDER BY total
   LIMIT 20;
+
+--21. ORDER BY Part II %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Write a query that returns the top 5 rows from orders ordered according to newest to oldest, but with the largest total_amt_usd for each date listed first for each date. You will notice each of these dates shows up as unique because of the time element. When you learn about truncating dates in a later lesson, you will better be able to tackle this question on a day, month, or yearly basis.
+*/
+
+SELECT total_amt_usd, occurred_at
+  FROM orders
+  ORDER BY occurred_at DESC, total_amt_usd DESC
+  LIMIT 5;
+
+/*
+2. Write a query that returns the top 10 rows from orders ordered according to oldest to newest, but with the smallest total_amt_usd for each date listed first for each date. You will notice each of these dates shows up as unique because of the time element. When you learn about truncating dates in a later lesson, you will better be able to tackle this question on a day, month, or yearly basis.
+*/
