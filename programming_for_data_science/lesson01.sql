2827ef868791e7b11513fd9a886d37ff4e9554e3
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 08:49:48 -1000

    Compl L01 Ex30 #01 Quiz: Arithmetic Operators

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -86,3 +86,16 @@ SELECT *
 SELECT name, website, primary_poc
   FROM accounts
   WHERE name = 'Exxon Mobil';
+
+--30. Quiz: Arithmetic Operators %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the results to the first 10 orders, and include the id and account_id fields.
+*/
+
+SELECT
+  id,
+  account_id,
+  standard_amt_usd / standard_qty AS standard_unit_price
+  FROM orders
+  LIMIT 10;
