27135fa444f78ed843889cbfc7fd92c272d3d9d8
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 11:49:57 -1000

    Compl L01 Ex46 #03 OR

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -263,3 +263,10 @@ SELECT *
 /*
 3. Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.
 */
+
+SELECT *
+  FROM accounts
+  WHERE (name LIKE 'C%' OR name LIKE 'W%')
+    AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%')
+    AND primary_poc NOT LIKE '%eana%'
+    ORDER BY name;
