33ad65fa63818d3cca480ef6fac949f0e5b1a92f
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 07:53:10 -1000

    Compl L01 Ex18 #03 ORDER BY

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -31,3 +31,8 @@ SELECT id, account_id, total_amt_usd
 /*
 Write a query to return the bottom 20 orders in terms of least total. Include the id, account_id, and total.
 */
+
+SELECT id, account_id, total
+  FROM orders
+  ORDER BY total
+  LIMIT 20;
