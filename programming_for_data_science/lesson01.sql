22bf9bc1b1f19e5554cc4319db92a23f943a3714
Sean Kagawa <seankagawa@gmail.com>
2018-10-29 08:34:42 -1000

    Compl L01 Ex27 #01 WHERE with Non-Numeric Data

--- programming_for_data_science/lesson01.sql
+++ programming_for_data_science/lesson01.sql
@@ -76,3 +76,13 @@ SELECT *
   FROM orders
   WHERE total_amt_usd < 500
   LIMIT 10;
+
+--27. WHERE with Non-Numeric Data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) for Exxon Mobil in the accounts table.
+*/
+
+SELECT name, website, primary_poc
+  FROM accounts
+  WHERE name = 'Exxon Mobil';
