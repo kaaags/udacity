1e4c6635b50d6979f2335f538de77294d767e0fb
Sean Kagawa <seankagawa@gmail.com>
2018-11-04 20:32:31 -1000

    Continued L02 Ex04 #01

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -4,3 +4,7 @@
 1. Try it yourself below by writing a query that limits the response to only the first 15 rows and includes the occurred_at, account_id, and channel fields in the web_events table.
 */
 
+SELECT orders.*
+  FROM orders
+  JOIN accounts
+  ON orders.account_id = accounts.id;
