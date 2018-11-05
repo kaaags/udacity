1bd5c6d3f78eae3702f2a745911365dd479957e5
Sean Kagawa <seankagawa@gmail.com>
2018-11-04 14:45:44 -1000

    Started L02 Ex04 #01

---
+++ programming_for_data_science/lesson02.sql
@@ -0,0 +1,6 @@
+--4. JOIN %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Try it yourself below by writing a query that limits the response to only the first 15 rows and includes the occurred_at, account_id, and channel fields in the web_events table.
+*/
+
