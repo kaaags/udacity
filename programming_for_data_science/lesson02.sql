40ef2858113d1e94e8aeb4c3896032a279ab02f8
Sean Kagawa <seankagawa@gmail.com>
2018-11-12 21:50:08 -1000

    Compl L02 Ex11 #02 Quiz: JOIN Questions Part I

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -54,7 +54,15 @@ SELECT a.primary_poc, w.occurred_at, w.channel, a.name
 2. Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
 */
 
-
+SELECT  r.name AS region_name,
+        s.name AS sales_rep_name,
+        a.name AS account_name
+    FROM sales_reps s
+      JOIN region r
+        ON s.region_id = r.id
+      JOIN accounts a
+        ON s.id = a.sales_rep_id
+  ORDER BY a.name;
 
 /*
 3. Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. Your final table should have 3 columns: region name, account name, and unit price. A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
