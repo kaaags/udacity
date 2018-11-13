f64fd0028c8bcd96b28f345e0230641abb6aac76
Sean Kagawa <seankagawa@gmail.com>
2018-11-12 21:17:21 -1000

    Edited L02 Ex11 #01 Quiz: JOIN Questions Part I

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -44,11 +44,11 @@ SELECT sales_reps.*, region.*
 1. Provide a table for all web_events associated with account name of Walmart. There should be three columns. Be sure to include the primary_poc, time of the event, and the channel for each event. Additionally, you might choose to add a fourth column to assure only Walmart events were chosen.
 */
 
-SELECT accounts.primary_poc, web_events.occurred_at, web_events.channel, accounts.name
-  FROM web_events
-    JOIN accounts
-      ON web_events.account_id = accounts.id
-    WHERE accounts.id = 1001;
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+  FROM web_events w
+    JOIN accounts a
+      ON w.account_id = a.id
+    WHERE a.id = 1001;
 
 /*
 2. Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
