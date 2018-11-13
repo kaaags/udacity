e03a4eba33c83f52f318ea17832cbab8e7d97b56
Sean Kagawa <seankagawa@gmail.com>
2018-11-12 16:14:03 -1000

    Compl L02 Ex04 #02.02

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -21,3 +21,8 @@ SELECT *
 /*
 2.2. Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website and the primary_poc from the accounts table.
 */
+
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.primary_poc
+  FROM orders
+  JOIN accounts
+  ON orders.account_id = accounts.id;
