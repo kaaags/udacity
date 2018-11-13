b92377e28b9e58cc7901cecb03dd07f8129ad0c3
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 10:57:37 -1000

    Compl L02 Ex19 #06 Quiz: Last Check

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -165,6 +165,20 @@ SELECT  r.name region_name,
 6. Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the largest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).
 */
 
+SELECT  r.name region_name,
+        a.name account_name,
+        o.total_amt_usd / (o.total + 0.0001) unit_price
+    FROM  region r
+      JOIN  sales_reps s
+        ON  r.id = s.region_id
+      JOIN  accounts a
+        ON  s.id = a.sales_rep_id
+      JOIN  orders o
+        ON  a.id = o.account_id
+  WHERE standard_qty > 100
+    AND poster_qty > 50
+  ORDER BY unit_price DESC;
+
 /*
 7. What are the different channels used by account id 1001? Your final table should have only 2 columns: account name and the different channels. You can try SELECT DISTINCT to narrow down the results to only the unique values.
 */
