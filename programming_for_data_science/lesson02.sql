52e7a2fc13e23a41793386203389e0d365796a49
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 11:18:50 -1000

    Edited L02 Ex19 #04 Quiz: Last Check

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -141,7 +141,8 @@ SELECT  r.name region_name,
       JOIN  accounts a
         ON  s.id = a.sales_rep_id
       JOIN  orders o
-        ON  a.id = o.account_id;
+        ON  a.id = o.account_id
+  WHERE o.standard_qty > 100;
 
 /*
 5. Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. However, you should only provide the results if the standard order quantity exceeds 100 and the poster order quantity exceeds 50. Your final table should have 3 columns: region name, account name, and unit price. Sort for the smallest unit price first. In order to avoid a division by zero error, adding .01 to the denominator here is helpful (total_amt_usd/(total+0.01).
