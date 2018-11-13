6eb478e8766e814f11bda20a74445643b4605428
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 10:29:50 -1000

    Compl L02 Ex19 #02 Quiz: Last Check

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -100,6 +100,17 @@ SELECT  r.name region_name,
 2. Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first name starting with S and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
 */
 
+SELECT  r.name region_name,
+        s.name sales_rep_name,
+        a.name account_name
+    FROM region r
+      JOIN sales_reps s
+        ON r.id = s.region_id
+      JOIN accounts a
+        ON s.id = a.sales_rep_id
+  WHERE s.name LIKE 'S%'
+    AND r.name IN ('Midwest')
+  ORDER BY a.name;
 
 /*
 3. Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a last name starting with K and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
