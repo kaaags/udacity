c399838a5682ce6c0d3b945eee3ff6c68a726850
Sean Kagawa <seankagawa@gmail.com>
2018-11-12 16:24:32 -1000

    Edited L02 Ex04 #02.01-02.02

--- programming_for_data_science/lesson02.sql
+++ programming_for_data_science/lesson02.sql
@@ -13,16 +13,16 @@ SELECT orders.*
 2.1. Try pulling all the data from the accounts table, and all the data from the orders table.
 */
 
-SELECT *
+SELECT orders.*, accounts.*
   FROM accounts
   JOIN orders
-  ON accounts.id = orders.id;
+  ON accounts.id = orders.account_id;
 
 /*
 2.2. Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website and the primary_poc from the accounts table.
 */
 
-SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.primary_poc
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.website, accounts.primary_poc
   FROM orders
   JOIN accounts
   ON orders.account_id = accounts.id;
