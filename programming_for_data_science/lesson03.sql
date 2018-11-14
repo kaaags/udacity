d41d995cfcf79594d4f4653eeecd3ee455a4bbc5
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 18:43:25 -1000

    Compl L03 Ex14 #07 Quiz: GROUP BY

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -186,3 +186,11 @@ SELECT  a.name,
 /*
 7. Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.
 */
+
+SELECT  r.name,
+        COUNT(s.name) AS num_reps
+    FROM  region r
+      JOIN  sales_reps s
+        ON  r.id = s.region_id
+  GROUP BY  r.name
+  ORDER BY  sales_reps;
