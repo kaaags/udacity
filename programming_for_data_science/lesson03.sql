a829d70ca3e2e7601cc52125a9b8a3459c18fd84
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 21:29:37 -1000

    Compl L03 Ex27 #01 Quiz: DATE Functions

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -466,3 +466,37 @@ SELECT  w.channel,
             a.name
   ORDER BY  use_by_accounts DESC
   LIMIT 1;
+
+--27. Quiz: DATE Functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. Do you notice any trends in the yearly sales totals?
+*/
+
+SELECT  DATE_TRUNC('year', o.occurred_at) AS year,
+        SUM(total_amt_usd) AS yearly_total
+    FROM  orders o
+  GROUP BY  DATE_TRUNC('year', o.occurred_at)
+  ORDER BY  yearly_total DESC;
+
+/*
+2. Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset?
+*/
+
+
+
+/*
+3. Which year did Parch & Posey have the greatest sales in terms of total number of orders? Are all years evenly represented by the dataset?
+*/
+
+
+
+/*
+4. Which month did Parch & Posey have the greatest sales in terms of total number of orders? Are all months evenly represented by the dataset?
+*/
+
+
+
+/*
+5. In which month of which year did Walmart spend the most on gloss paper in terms of dollars?
+*/
