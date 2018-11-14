b3f7224a2f4c2f8252148b9ff3da7e4f2180335b
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 17:36:00 -1000

    Compl L03 Ex14 #01 Quiz: GROUP BY

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -110,3 +110,51 @@ SELECT  o.total_amt_usd AS median_total_amt_usd
     FROM  orders o
   ORDER BY  o.total_amt_usd
   LIMIT 1 OFFSET 3456;
+
+--14. Quiz: GROUP BY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+*/
+
+SELECT  a.name,
+        o.occurred_at
+    FROM  accounts a
+      JOIN  orders o
+        ON  a.id = o.account_id
+  ORDER BY  occurred_at
+  LIMIT 1;
+
+/*
+2. Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
+*/
+
+
+
+/*
+3. Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
+*/
+
+
+
+/*
+4. Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+*/
+
+
+
+/*
+5. Who was the primary contact associated with the earliest web_event?
+*/
+
+
+
+/*
+6. What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+*/
+
+
+
+/*
+7. Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.
+*/
