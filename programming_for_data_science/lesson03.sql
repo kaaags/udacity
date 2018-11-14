2cb3ab338fd67dca03c9fbe0ade92dd17490b680
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 15:04:57 -1000

    Edited L03 #01 Quiz: SUM

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -4,26 +4,27 @@
 1. Find the total amount of poster_qty paper ordered in the orders table.
 */
 
-
+SELECT  SUM(o.poster_qty) AS poster_total
+    FROM  orders o;
 
 /*
-Find the total amount of standard_qty paper ordered in the orders table.
+2. Find the total amount of standard_qty paper ordered in the orders table.
 */
 
 
 
 /*
-Find the total dollar amount of sales using the total_amt_usd in the orders table.
+3. Find the total dollar amount of sales using the total_amt_usd in the orders table.
 */
 
 
 
 /*
-Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table.
+4. Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table.
 */
 
 
 
 /*
-Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both an aggregation and a mathematical operator.
+5. Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both an aggregation and a mathematical operator.
 */
