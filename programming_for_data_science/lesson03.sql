4cdb454b9ab739f3336fb678c68c638efedc1d6f
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 19:12:06 -1000

    Compl L03 Ex17 #04 Quiz: GROUP BY

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -241,3 +241,17 @@ SELECT  s.name,
 /*
 4. Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
 */
+
+SELECT  r.name,
+        w.channel,
+        COUNT(w.channel) AS channel_occurrences
+    FROM  region r
+      JOIN  sales_reps s
+        ON  r.id = s.region_id
+      JOIN  accounts a
+        ON  s.id = a.sales_rep_id
+      JOIN  web_events w
+        ON  a.id = w.account_id
+  GROUP BY  r.name,
+            w.channel
+  ORDER BY  channel_occurrences DESC;
