b4a66745edbe84226967e263ccd141fe42c9c898
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 20:23:58 -1000

    Compl L03 Ex23 #04 Quiz: HAVING

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -359,7 +359,17 @@ SELECT  a.id,
 4. How many accounts spent more than 30,000 usd total across all orders?
 */
 
-
+SELECT  COUNT(*)  num_accounts_above30k_usd
+    FROM( SELECT  a.id,
+                  a.name,
+                  COUNT(*) total_spent
+              FROM  accounts a
+                JOIN  orders o
+                  ON  a.id = o.account_id
+            GROUP BY  a.id,
+                      a.name
+            HAVING  SUM(o.total_amt_usd) > 30000
+            ORDER BY total_spent) AS Table1;
 
 /*
 5. How many accounts spent less than 1,000 usd total across all orders?
