2c2cd94055915a4d2e902402f0c8844120030266
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 18:36:46 -1000

    Compl L03 Ex14 #06 Quiz: GROUP BY

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -175,7 +175,13 @@ SELECT  a.primary_poc
 6. What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
 */
 
-
+SELECT  a.name,
+        MIN(o.total_amt_usd) AS smallest_order
+    FROM  accounts a
+      JOIN  orders o
+        ON  a.id = o.account_id
+  GROUP BY  a.name
+  ORDER BY  smallest_order;
 
 /*
 7. Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.
