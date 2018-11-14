f945e324a2767d78a2358f20288024b92f0e8f0e
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 15:35:15 -1000

    Compl L03 #03 Quiz: SUM

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -18,7 +18,8 @@ SELECT  SUM(o.standard_qty) AS standard_total
 3. Find the total dollar amount of sales using the total_amt_usd in the orders table.
 */
 
-
+SELECT  SUM(o.total_amt_usd) AS grand_total_amt_usd
+    FROM  orders o;
 
 /*
 4. Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table.
