3a1d083ab4a9a01ba6db92cf1298837a7167a70a
Sean Kagawa <seankagawa@gmail.com>
2018-11-13 22:46:48 -1000

    Compl L03 Ex31 #03 Quiz: CASE

--- programming_for_data_science/lesson03.sql
+++ programming_for_data_science/lesson03.sql
@@ -573,7 +573,21 @@ SELECT  CASE
 3. We would like to understand 3 different levels of customers based on the amount associated with their purchases. The top level includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd. The second level is between 200,000 and 100,000 usd. The lowest level is anyone under 100,000 usd. Provide a table that includes the level associated with each account. You should provide the account name, the total sales of all orders for the customer, and the level. Order with the top spending customers listed first.
 */
 
-
+SELECT  a.name,
+        SUM(o.total_amt_usd) total_sales,
+        CASE
+          WHEN  SUM(o.total_amt_usd) > 200000
+            THEN  'Top Level'
+          WHEN  SUM(o.total_amt_usd) > 100000
+            AND SUM(o.total_amt_usd) <= 200000
+            THEN  'Mid Level'
+          ELSE  'Bottom Level'
+          END AS  customer_level
+    FROM  orders o
+      JOIN  accounts a
+        ON  o.account_id = a.id
+  GROUP BY  1
+  ORDER BY  2 DESC;
 
 /*
 4. We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending customers listed first.
