88ab8f96d9c8ccd3f22922a91ea5b5e739462132
Sean Kagawa <seankagawa@gmail.com>
2018-11-25 22:10:06 -1000

    Compl L04 Ex14 #03 Quiz: WITH

--- programming_for_data_science/lesson04.sql
+++ programming_for_data_science/lesson04.sql
@@ -274,7 +274,23 @@ SELECT r.name region_name, COUNT(o.total) tot_orders
 3. For the name of the account that purchased the most (in total over their lifetime as a customer) standard_qty paper, how many accounts still had more in total purchases?
 */
 
-
+WITH t1 AS (
+  SELECT a.name account_name, SUM(o.standard_qty) tot_std, SUM(o.total) tot
+    FROM accounts a
+    JOIN orders o
+    ON a.id = o.account_id
+    GROUP BY 1
+    ORDER BY 2 DESC
+    LIMIT 1),
+  t2 AS (
+    SELECT a.name
+      FROM orders o
+      JOIN accounts a
+      ON a.id = o.account_id
+      GROUP BY 1
+      HAVING SUM(o.total) > (SELECT tot FROM t1))
+SELECT COUNT(*)
+  FROM t2;
 
 /*
 4. For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, how many web_events did they have for each channel?
