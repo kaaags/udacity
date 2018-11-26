d41c280b5656d6793a7f1a16d80d115ef1e8de2d
Sean Kagawa <seankagawa@gmail.com>
2018-11-25 15:47:16 -1000

    Edited L04 Ex07 #05 Quiz: Subquery Mania

--- programming_for_data_science/lesson04.sql
+++ programming_for_data_science/lesson04.sql
@@ -167,7 +167,7 @@ SELECT COUNT(*) count
                   ON a.id = o.account_id
                   GROUP BY 1
                   ORDER BY 2 DESC
-                LIMIT 1) inner_tab)
+                  LIMIT 1) inner_tab)
         ) counter_tab;
 
 /*
