3b487fddc3c1d8d81c4dabe222f86135fd90f4b0
Sean Kagawa <seankagawa@gmail.com>
2019-01-03 08:29:39 -1000

    Compl L05 Ex15 #06 Quiz: COALESCE

--- programming_for_data_science/lesson05.sql
+++ programming_for_data_science/lesson05.sql
@@ -240,3 +240,29 @@ SELECT  COUNT(*)
 FROM accounts a
 LEFT JOIN orders o
 ON a.id = o.account_id;
+
+/*
+6. Run the query in 5, but with the COALESCE function used in questions 2 through 4.
+*/
+
+SELECT  COALESCE(a.id, a.id) filled_id,
+        a.name,
+        a.website,
+        a.lat,
+        a.long,
+        a.primary_poc,
+        a.sales_rep_id,
+        o.id,
+        COALESCE(o.account_id, a.id) account_id,
+        o.occurred_at,
+        COALESCE(o.standard_qty, 0) standard_qty,
+        COALESCE(o.gloss_qty, 0) gloss_qty,
+        COALESCE(o.poster_qty, 0) poster_qty,
+        COALESCE(o.total, 0) total,
+        COALESCE(o.standard_amt_usd, 0) standard_amt_usd,
+        COALESCE(o.gloss_amt_usd, 0) gloss_amt_usd,
+        COALESCE(o.poster_amt_usd, 0) poster_amt_usd,
+        COALESCE(o.total_amt_usd, 0) total_amt_usd
+  FROM  accounts a
+  LEFT JOIN orders o
+  ON  a.id = o.account_id;
