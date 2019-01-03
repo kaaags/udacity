47a17734c90b955a2e5778985f0d4cb62d03bd51
Sean Kagawa <seankagawa@gmail.com>
2019-01-02 15:30:15 -1000

    Compl L05 Ex03 #01 Quiz: LEFT & RIGHT

---
+++ programming_for_data_science/lesson05.sql
@@ -0,0 +1,11 @@
+--5. SQL Data Cleaning %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/*
+1. In the accounts table, there is a column holding the website for each company. The last three digits specify what type of web address they are using. A list of extensions (and pricing) is provided here. Pull these extensions and provide how many of each website type exist in the accounts table.
+*/
+
+SELECT  RIGHT(a.website, 3) AS domain,
+        COUNT(*) num_companies
+  FROM accounts a
+  GROUP BY 1
+  ORDER BY 2;
