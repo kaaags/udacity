876cbcaa69d0a81fb0dc60ccf171e68e64a7fc7c
Sean Kagawa <seankagawa@gmail.com>
2019-01-02 16:57:19 -1000

    Compl L05 Ex09 #02 Quiz: CONCAT

--- programming_for_data_science/lesson05.sql
+++ programming_for_data_science/lesson05.sql
@@ -99,6 +99,16 @@ SELECT  first_name,
 2. You may have noticed that in the previous solution some of the company names include spaces, which will certainly not work in an email address. See if you can create an email address that will work by removing all of the spaces in the account name, but otherwise your solution should be just as in question 1. Some helpful documentation is here.
 */
 
+WITH t1 AS (
+  SELECT  LEFT(a.primary_poc, STRPOS(a.primary_poc, ' ') -1) first_name,
+          RIGHT(a.primary_poc, LENGTH(a.primary_poc) - STRPOS(a.primary_poc, ' ')) last_name,
+          a.name
+    FROM accounts a)
+SELECT  first_name,
+        last_name,
+        CONCAT(first_name,'.',last_name,'@',REPLACE(name,' ',''),'.com') email_address
+  FROM t1;
+
 /*
 3. We would also like to create an initial password, which they will change after their first log in. The first password will be the first letter of the primary_poc's first name (lowercase), then the last letter of their first name (lowercase), the first letter of their last name (lowercase), the last letter of their last name (lowercase), the number of letters in their first name, the number of letters in their last name, and then the name of the company they are working with, all capitalized with no spaces.
 */
